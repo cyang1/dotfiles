e40556a6037984c4a435ffc6dd18fb4a336c9bd9
Cary Yang <caryy@cmu.edu>
2015-06-17 10:25:23 -0700

    Useful MySQL promptline.

--- system/env.zsh
+++ system/env.zsh
@@ -3,6 +3,7 @@ export EDITOR='vim'
 export PAGER='less'
 export LESS='-WR'
 export LC_CTYPE=$LANG
+export MYSQL_PS1="\u@\h [\d]>"
 
 # Disable the (venv) before the prompt
 export VIRTUAL_ENV_DISABLE_PROMPT='1'
